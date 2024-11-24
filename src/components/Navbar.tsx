16993bcd381d0fcfda0b5b009d962997ebd4a997
Wesley <wesleypersilva@gmail.com>
2024-11-24 13:33:41 -0300

    fix navbar dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
           <Link to="/" className="btn btn-ghost normal-case text-xl">Big-O Battle</Link>
         </div>
         <div className="flex-none lg:hidden">
-          <div className="dropdown">
+          <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
                    viewBox="0 0 24 24" stroke="currentColor">
